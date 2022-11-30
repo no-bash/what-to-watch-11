a52fb86c08bf098440a96a11707b4ab5cb97cb08
no-bash <blunskay@gmail.com>
2022-11-30 21:49:22 +0400

    fix

--- project/src/components/HeadGuest.tsx
+++ project/src/components/HeadGuest.tsx
@@ -1,30 +1,26 @@
-import React from "react";
+const HeadGuest = () => (
+  <section className='film-card'>
+    <div className='film-card__bg'>
+      {/*<img src='img/bg-the-grand-budapest-hotel.jpg' alt='The Grand Budapest Hotel' />*/}
+      <img src={'img/bg-header.jpg'}/>
+    </div>
 
-const HeadGuest = () => {
+    <h1 className='visually-hidden'>WTW</h1>
 
-  return(
-    <section className="film-card">
-      <div className="film-card__bg">
-        {/*<img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />*/}
-        <img src="img/bg-header.jpg"/>
+    <header className='page-header'>
+      <div className='logo'>
+        <a className='logo__link'>
+          <span className='logo__letter logo__letter--1'>W</span>
+          <span className='logo__letter logo__letter--2'>T</span>
+          <span className='logo__letter logo__letter--3'>W</span>
+        </a>
       </div>
 
-      <h1 className="visually-hidden">WTW</h1>
+      <div className='user-block'>
+        <a href={'sign-in.html'} className='user-block__link'>Sign in</a>
+      </div>
+    </header>
 
-      <header className="page-header">
-        <div className="logo">
-          <a className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
-        <div className="user-block">
-          <a href="sign-in.html" className="user-block__link">Sign in</a>
-        </div>
-      </header>
-
-    </section>
-  )
-}
+  </section>
+);
+export default HeadGuest;
